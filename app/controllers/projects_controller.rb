f8595c505a6ecb0bf2e716ee267a31a5ec48c5f4
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-13 21:22:10 +0100

    Projets : index, show, CRUD et style

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,32 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+    if @project.save
+      redirect_to @project, notice: "Nickel, le projet a été ajouté !"
+    else
+      render 'new', notice: "Ah mince, le projet n'a pas été enregistré !"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+end
