de829d4ad4aa7b199fc7aa3938e59b78e4a86f65
Tony Boucheau <boucheau.tony@neuf.fr>
2017-02-17 02:58:14 +0100

    bug d'update de post résolu

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectsController < ApplicationController
+
   before_action :find_project, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
   end
@@ -36,6 +38,7 @@ class ProjectsController < ApplicationController
     @project.destroy
     redirect_to projects_path
   end
+  
   private
 
   def project_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
- resources :posts
- resources :projects
- resources :contacts, only: [:new, :create]
- get 'welcome/index'
- root 'welcome#index'
- get '*path' => redirect('/')
+  resources :posts
+  resources :projects
+  resources :contacts, only: [:new, :create]
+  get 'welcome/index'
+  root 'welcome#index'
+  get '*path' => redirect('/')
 end
